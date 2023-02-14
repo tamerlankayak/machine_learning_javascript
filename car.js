430d427477ec533aaf990a394699a074e9abbfc6
Tamerlan <tamerlan.mustafayev@rabita.az>
2023-02-14 16:30:33 +0400

    driving well with law

--- car.js
+++ car.js
@@ -10,15 +10,51 @@ class Car {
     this.maxSpeed = 3;
     this.friction = 0.05;
     this.angle = 0;
+    this.damaged = false;
 
     this.sensor = new Sensor(this);
-
     this.controls = new Controls();
   }
 
-  update() {
-    this.#move();
-    this.sensor.update();
+  update(roadBorders) {
+    if (!this.damaged) {
+      this.#move();
+      this.polygon = this.#createPolygon();
+      this.damaged = this.#assessDamage(roadBorders);
+    }
+    this.sensor.update(roadBorders);
+  }
+
+  #assessDamage(roadBorders) {
+    for (let i = 0; i < roadBorders.length; i++) {
+      if (polysIntersect(this.polygon, roadBorders[i])) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  #createPolygon() {
+    const points = [];
+    const rad = Math.hypot(this.width, this.height) / 2;
+    const alpha = Math.atan2(this.width, this.height);
+    points.push({
+      x: this.x - Math.sin(this.angle - alpha) * rad,
+      y: this.y - Math.cos(this.angle - alpha) * rad,
+    });
+    points.push({
+      x: this.x - Math.sin(this.angle + alpha) * rad,
+      y: this.y - Math.cos(this.angle + alpha) * rad,
+    });
+    points.push({
+      x: this.x - Math.sin(Math.PI + this.angle - alpha) * rad,
+      y: this.y - Math.cos(Math.PI + this.angle - alpha) * rad,
+    });
+    points.push({
+      x: this.x - Math.sin(Math.PI + this.angle + alpha) * rad,
+      y: this.y - Math.cos(Math.PI + this.angle + alpha) * rad,
+    });
+    return points;
   }
 
   #move() {
@@ -28,33 +64,24 @@ class Car {
     if (this.controls.reverse) {
       this.speed -= this.acceleration;
     }
+
     if (this.speed > this.maxSpeed) {
       this.speed = this.maxSpeed;
     }
     if (this.speed < -this.maxSpeed / 2) {
       this.speed = -this.maxSpeed / 2;
     }
+
     if (this.speed > 0) {
       this.speed -= this.friction;
     }
     if (this.speed < 0) {
       this.speed += this.friction;
-    }
-    if (this.speed > this.maxSpeed) {
-      this.speed = this.maxSpeed;
-    }
-    if (this.speed < -this.maxSpeed / 2) {
-      this.speed = -this.maxSpeed / 2;
-    }
-    if (this.speed > 0) {
-      this.speed -= this.friction;
     }
     if (Math.abs(this.speed) < this.friction) {
       this.speed = 0;
     }
-    if (this.speed < 0) {
-      this.speed += this.friction;
-    }
+
     if (this.speed != 0) {
       const flip = this.speed > 0 ? 1 : -1;
       if (this.controls.left) {
@@ -70,15 +97,17 @@ class Car {
   }
 
   draw(ctx) {
-    ctx.save();
-    ctx.translate(this.x, this.y);
-    ctx.rotate(-this.angle);
-
+    if (this.damaged) {
+      ctx.fillStyle = "gray";
+    } else {
+      ctx.fillStyle = "black";
+    }
     ctx.beginPath();
-    ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
+    ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
+    for (let i = 1; i < this.polygon.length; i++) {
+      ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
+    }
     ctx.fill();
-
-    ctx.restore();
 
     this.sensor.draw(ctx);
   }
--- main.js
+++ main.js
@@ -1,22 +1,23 @@
-const canvas = document.getElementById("myCanvas");
-canvas.width = 200;
+const canvas=document.getElementById("myCanvas");
+canvas.width=200;
 
 const ctx = canvas.getContext("2d");
-const road = new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50);
+const road=new Road(canvas.width/2,canvas.width*0.9);
+const car=new Car(road.getLaneCenter(1),100,30,50);
 
 animate();
 
-function animate() {
-  car.update();
-  canvas.height = window.innerHeight;
+function animate(){
+    car.update(road.borders);
 
-  ctx.save();
-  ctx.translate(0, -car.y + canvas.height * 0.7);
+    canvas.height=window.innerHeight;
 
-  road.draw(ctx);
-  car.draw(ctx);
+    ctx.save();
+    ctx.translate(0,-car.y+canvas.height*0.7);
 
-  ctx.restore();
-  requestAnimationFrame(animate);
+    road.draw(ctx);
+    car.draw(ctx);
+
+    ctx.restore();
+    requestAnimationFrame(animate);
 }
